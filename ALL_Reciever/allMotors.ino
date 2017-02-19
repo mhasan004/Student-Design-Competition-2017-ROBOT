43e70b60c5d981dc036008016f9a53747c4220bf
MahmudulHasan0 <mhasan1997@yahoo.com>
2017-02-19 02:36:19 -0500

    Add files via upload

---
+++ ALL_Reciever/allMotors.ino
@@ -0,0 +1,40 @@
+void ALL_MOTORS(){
+  LeftMotor1.write(Motor1);
+  LeftMotor2.write(Motor2);
+  LeftMotor3.write(Motor3);  
+  RightMotor4.write(Motor4);                
+  RightMotor5.write(Motor5);
+  RightMotor6.write(Motor6);
+}
+
+
+/*void ALL_MOTORS_Y(){
+  LeftMotor1.write(Motor3_y);
+  LeftMotor2.write(Motor2_y);
+  LeftMotor3.write(Motor1_y);
+  RightMotor1.write(Motor6_y);                
+  RightMotor5.write(Motor5_y);
+  RightMotor6.write(Motor4_y);
+}
+void ALL_MOTORS_X(){
+  LeftMotor1.write(Motor3_x);
+  LeftMotor2.write(Motor2_x);
+  LeftMotor3.write(Motor1_x);
+  RightMotor1.write(Motor6_x);                
+  RightMotor5.write(Motor5_x);
+  RightMotor6.write(Motor4_x);
+}
+
+
+void LEFT_MOTORS_X(){
+  LeftMotor1.write(Motor3_x);
+  LeftMotor2.write(Motor2_x);
+  LeftMotor3.write(Motor1_x);
+}
+void RIGHT_MOTORS_X(){
+  RightMotor1.write(Motor6_x);                
+  RightMotor5.write(Motor5_x);
+  RightMotor6.write(Motor4_x);  
+}
+*/
+
