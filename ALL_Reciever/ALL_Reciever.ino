db725e0cfea071d7dae1e4c94a411039f68c19d8
MahmudulHasan0 <mhasan1997@yahoo.com>
2018-04-24 15:36:26 -0400

    Update ALL_Reciever.ino

--- ALL_Reciever/ALL_Reciever.ino
+++ ALL_Reciever/ALL_Reciever.ino
@@ -64,8 +64,6 @@ void setup(){
   radio.startListening(); 
 }//End of Setup
 
-
-
 unsigned long lastRecvTime = 0;
 void loop() 
 {
@@ -120,6 +118,43 @@ void loop()
   Serial.println();      
   radio.startListening(); 
 }
+
+////////////////:FUNCTIONS://///////////////
+void ALL_MOTORS(){
+  LeftMotor1.write(Motor1);
+  LeftMotor2.write(Motor2);
+  LeftMotor3.write(Motor3);  
+  RightMotor4.write(Motor4);                
+  RightMotor5.write(Motor5);
+  RightMotor6.write(Motor6);
+}
+/*void ALL_MOTORS_Y(){
+  LeftMotor1.write(Motor3_y);
+  LeftMotor2.write(Motor2_y);
+  LeftMotor3.write(Motor1_y);
+  RightMotor1.write(Motor6_y);                
+  RightMotor5.write(Motor5_y);
+  RightMotor6.write(Motor4_y);
+}
+void ALL_MOTORS_X(){
+  LeftMotor1.write(Motor3_x);
+  LeftMotor2.write(Motor2_x);
+  LeftMotor3.write(Motor1_x);
+  RightMotor1.write(Motor6_x);                
+  RightMotor5.write(Motor5_x);
+  RightMotor6.write(Motor4_x);
+}
+void LEFT_MOTORS_X(){
+  LeftMotor1.write(Motor3_x);
+  LeftMotor2.write(Motor2_x);
+  LeftMotor3.write(Motor1_x);
+}
+void RIGHT_MOTORS_X(){
+  RightMotor1.write(Motor6_x);                
+  RightMotor5.write(Motor5_x);
+  RightMotor6.write(Motor4_x);  
+}
+*/
 
 
 
